e3394b110d20b04c23f62447c19e98b6a8bb1a4a
nki1121 <nkiuniv@gmail.com>
2024-04-12 13:54:25 +0900

    BView.swiftの作成

---
+++ Part8/BView.swift
@@ -0,0 +1,20 @@
+//
+//  BView.swift
+//  Part8
+//
+//  Created by 中井晋平 on 2024/04/12.
+//
+
+import SwiftUI
+
+struct BView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BView_Previews: PreviewProvider {
+    static var previews: some View {
+        BView()
+    }
+}
