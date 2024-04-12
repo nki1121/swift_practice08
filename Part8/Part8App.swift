2e4188f8e58a82dc5163393b6bec5d6eb0e12802
nki1121 <nkiuniv@gmail.com>
2024-04-12 13:51:59 +0900

    Initial Commit

---
+++ Part8/Part8App.swift
@@ -0,0 +1,17 @@
+//
+//  Part8App.swift
+//  Part8
+//
+//  Created by 中井晋平 on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct Part8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
