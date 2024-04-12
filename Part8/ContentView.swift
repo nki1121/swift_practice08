e5db88cab42e3f5a12aea7ba768a9b02e4d34bff
nki1121 <nkiuniv@gmail.com>
2024-04-12 14:08:36 +0900

    @Bindingに値を渡す

--- Part8/ContentView.swift
+++ Part8/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .font(.largeTitle)
         }
         .sheet(isPresented: $isShowBView) {
-            BView()
+            BView(isShowBView: $isShowBView)
         }
     }
 }
